e812a44ad8952f31f52fea46793d084b2af84980
hagyuho <hagyuho90@gmail.com>
2021-06-05 23:29:38 +0900

    like jojoldu day6

--- src/main/java/com/hadoyaji/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/hadoyaji/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.hadoyaji.book.springboot.config.auth.dto;
 import com.hadoyaji.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
