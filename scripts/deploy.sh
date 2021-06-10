f158d3c6970d98bd2a23479896ffe90590da6337
hagyuho <hagyuho90@gmail.com>
2021-06-10 10:32:54 +0900

    4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,17 +8,16 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">현재 구동 중인 애플리케이션 pid 확인"
 
-CRUUENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
 
 echo ">현재 구동 중인 애플리케이션 pid 확인: $CRUUENT_PID"
 
-if[ -z "$CURRENT_PID"]; then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CRUUENT_PID
-  sleep 5
-
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
@@ -28,7 +27,7 @@ echo ">JAR NAME : $JAR_NAME"
 
 echo ">JAR NAME에 실행권한 추가"
 
-chmod + x $JAR_JAR_NAME
+chmod +x $JAR_NAME
 
 echo ">JAR NAME 실행"
 
